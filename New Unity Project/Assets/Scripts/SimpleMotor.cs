9fb035c1997f24c88aa393b23fe78de0cf4ded98
yesaa <debrab_c14@myges.fr>
2016-05-11 23:16:13 +0200

    new iteration

---
+++ New Unity Project/Assets/Scripts/SimpleMotor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMotor : MonoBehaviour {
+
+  public float speed = 5;
+  //public bool useLocalDirection;
+  //public Vector3 direction = Vector3.zero;
+
+	public GameObject Player;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+    	//if(useLocalDirection) transform.position += transform.TransformDirection(direction) * speed * Time.deltaTime;
+    	//else transform.position += direction * speed * Time.deltaTime;
+
+
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		transform.position += Player.transform.forward * speed * Time.deltaTime;
+	}
+}
